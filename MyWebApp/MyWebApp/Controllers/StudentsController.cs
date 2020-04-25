b6d67efba0a78dc75fa8f83841986efcbdaa7f0c
Jan Rejnowski <jan.rejnowski@gmail.com>
2020-04-25 18:21:22 +0200

    c04 done

--- MyWebApp/MyWebApp/Controllers/StudentsController.cs
+++ MyWebApp/MyWebApp/Controllers/StudentsController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MyWebApp.Models;
 using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
 
 namespace MyWebApp.Controllers
 {
@@ -8,24 +10,86 @@ namespace MyWebApp.Controllers
     [Route("api/students")]
     public class StudentsController : ControllerBase
     {
+        private const string ConnectionString = "Data Source=db-mssql;Initial Catalog=s18410;Integrated Security=True";
         [HttpGet]
-        public string GetStudents(string orderBy)
+        public IActionResult GetStudents(string orderBy)
         {
-            return $"Kowalski, Malewski, Andrzejewski sortowanie={orderBy}";
+            var list = new List<Student>();
+            using(var con = new SqlConnection(ConnectionString))
+            using (var com = new SqlCommand())
+            {
+                com.Connection = con;
+                com.CommandText = "select FirstName, LastName, BirthDate, Name FROM Student JOIN Enrollment ON Student.IdEnrollment = Enrollment.IdEnrollment JOIN Studies ON Enrollment.IdStudy = Studies.IdStudy;";
+                
+                con.Open();
+                var dr = com.ExecuteReader();
+                while(dr.Read())
+                {
+                    var st = new Student();
+                    st.BirthDate = DateTime.Parse(dr["BirthDate"].ToString());
+                    st.FirstName = dr["FirstName"].ToString();
+                    st.LastName = dr["LastName"].ToString();
+                    st.StudiesName = dr["Name"].ToString();
+                    list.Add(st);
+                }
+            }
+            return Ok(list);
         }
 
-        [HttpGet("{id}")]
-        public IActionResult GetStudent(int id)
+        [HttpGet("{indexNumber}")]
+        public IActionResult GetStudent(string indexNumber)
         {
-            if (id == 1)
+            var list = new List<Student>();
+            using (var con = new SqlConnection(ConnectionString))
+            using (var com = new SqlCommand())
             {
-                return Ok("Kowalski");
-            } else if (id == 2)
-            {
-                return Ok("Malewski");
-            }
+                com.Connection = con;
+                com.CommandText = "select * from Student where IndexNumber=@index";
+                com.Parameters.AddWithValue("index", indexNumber);
 
-            return NotFound("Nie znaleziono studenta");
+                con.Open();
+                var dr = com.ExecuteReader();
+                if (dr.Read())
+                {
+                    var st = new Student();
+                    st.IndexNumber = dr["IndexNumber"].ToString();
+                    st.FirstName = dr["FirstName"].ToString();
+                    st.LastName = dr["LastName"].ToString();
+                    list.Add(st);
+                    return Ok(list);
+                }
+            }
+            return NotFound();
+        }
+
+        [HttpGet("enrollments/{indexNumber}")]
+        public IActionResult GetStudentEnrollment(string indexNumber)
+        {
+            var list = new List<Enrollment>();
+            using(var con = new SqlConnection(ConnectionString))
+            using(var com = new SqlCommand())
+            {
+                com.Connection = con;
+                com.CommandText = "select Enrollment.IdEnrollment, Semester, IdStudy, StartDate FROM Enrollment JOIN Student ON Student.IdEnrollment = Enrollment.IdEnrollment WHERE IndexNumber=@indexNumber";
+                com.Parameters.AddWithValue("indexNumber", indexNumber);
+
+                con.Open();
+                var dr = com.ExecuteReader();
+                while (dr.Read())
+                {
+                    var enrollment = new Enrollment();
+                    enrollment.IdEnrollment = Int32.Parse(dr["IdEnrollment"].ToString());
+                    enrollment.IdStudy = Int32.Parse(dr["IdStudy"].ToString());
+                    enrollment.Semester = Int32.Parse(dr["Semester"].ToString());
+                    enrollment.StartDate = DateTime.Parse(dr["StartDate"].ToString());
+
+                    list.Add(enrollment);
+                }
+            }
+            if (list.Count > 0)
+                return Ok(list);
+            else
+                return NotFound();
         }
 
         [HttpPost]
